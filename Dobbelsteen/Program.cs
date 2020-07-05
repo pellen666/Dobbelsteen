8a51f550a91bc2563ea91f30c24e7236686211f8
paul <paulsinterniklaas@hotmail.com>
2020-07-05 21:15:29 +0200

    Add project files.

---
+++ Dobbelsteen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dobbelsteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
